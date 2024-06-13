d29954abbe675b6a6075445d945cf8a7a8cd3139
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-13 14:11:05 -0400

    Display all task in list

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Task() {
+
+function Task({text, category}) {
+  const [task, setTask] = useState("")
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
       <button className="delete">X</button>
     </div>
   );
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks}) {
+  const taskList = tasks.map((task) => {
+    console.log("taskList", task)
+    return (
+      <Task
+        key={task.text}
+        text={task.text}
+        category={task.category}
+      />
+    )
+  })
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskList}
     </div>
   );
 }
